cef0654343e2ba384679e27612d10cfdbe321c05
Jared Moulton <jaredmoulton3@gmail.com>
2021-11-03 18:09:05 -0600

    Update to simpler instantiaiton

--- DoubleSevenSegment.srcs/sources_1/imports/jaredmoulton/Decoder/Decoder.srcs/sources_1/new/decoder3_8.v
+++ DoubleSevenSegment.srcs/sources_1/imports/jaredmoulton/Decoder/Decoder.srcs/sources_1/new/decoder3_8.v
@@ -20,24 +20,25 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module decoder3_8(d, en, o);
-	input [2:0] d;
-	input en;
-	output reg [0:7] o;
+module decoder3_8(
+input [2:0] d,
+input en,
+output reg [0:7] o
+);
 
-	always @(*) begin
-		if (en) begin
-			case(d)
-				3'd7: o=8'd1;
-				3'd6: o=8'd2;
-				3'd5: o=8'd4;
-				3'd4: o=8'd8;
-				3'd3: o=8'd16;
-				3'd2: o=8'd32;
-				3'd1: o=8'd64;
-				3'd0: o=8'd128;
-			endcase
-		end
-		else o=8'd0;
-	end
+always @(*) begin
+    if (en) begin
+        case(d)
+            3'd7: o=8'd1;
+            3'd6: o=8'd2;
+            3'd5: o=8'd4;
+            3'd4: o=8'd8;
+            3'd3: o=8'd16;
+            3'd2: o=8'd32;
+            3'd1: o=8'd64;
+            3'd0: o=8'd128;
+        endcase
+    end
+    else o=8'd0;
+end
 endmodule
